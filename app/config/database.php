ed8a41ce1042dac151fbf5ec216385266e56aed6
AlexStorm13 <alexbrasser@gmail.com>
2014-10-08 13:47:36 +0200

    database file geupdate

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'stage.alexbrasser.nl',
+			'username'  => 'root',
+			'password'  => 'alex13',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
