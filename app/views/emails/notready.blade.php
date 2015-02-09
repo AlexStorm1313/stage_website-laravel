a7869244262f24084b63760c7de02d30eff5e7e6
AlexStorm13 <alexbrasser@gmail.com>
2015-02-09 08:42:11 +0100

    added email template

--- app/views/emails/notready.blade.php
+++ app/views/emails/notready.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en-US">
-	<head>
-		<meta charset="utf-8">
-	</head>
-	<body>
-		<h2>Week not filled</h2>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+<h2>Week not filled</h2>
 
-		<div>
-			The intern coach tried to complete a week but it is not completely filled
-		</div>
-	</body>
+<div>
+	The intern coach tried to complete a week but it is not completely filled
+</div>
+</body>
 </html>
